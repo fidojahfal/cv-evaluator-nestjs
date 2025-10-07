05e121ca64a711e8d856fefd36fcb7faf6eda1fc
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-08 02:17:20 +0700

    feat: create new file required for handling evaluate

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { UploadModule } from './upload/upload.module';
+import { EvaluateModule } from './evaluate/evaluate.module';
 
 @Module({
-  imports: [CommonModule, UploadModule],
+  imports: [CommonModule, UploadModule, EvaluateModule],
   controllers: [AppController],
   providers: [AppService],
 })

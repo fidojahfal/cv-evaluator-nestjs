ca783fb606e1187419133118ba6c9b5a91312952
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-08 16:19:00 +0700

    feat: implement error filter to handle error from zod and else

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,6 +2,8 @@ import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma/prisma.service';
 import { ConfigModule } from '@nestjs/config';
 import { ValidationService } from './validation/validation.service';
+import { ErrorFilter } from './filter/error.filter';
+import { APP_FILTER } from '@nestjs/core';
 
 @Global()
 @Module({
@@ -10,7 +12,14 @@ import { ValidationService } from './validation/validation.service';
       isGlobal: true,
     }),
   ],
-  providers: [PrismaService, ValidationService],
-  exports: [PrismaService],
+  providers: [
+    PrismaService,
+    ValidationService,
+    {
+      provide: APP_FILTER,
+      useClass: ErrorFilter,
+    },
+  ],
+  exports: [PrismaService, ValidationService],
 })
 export class CommonModule {}

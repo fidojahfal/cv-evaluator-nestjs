65d5fa1ac5599ed08314a507a4863374497f236c
fidojahfal <fidojahfalprayoga@gmail.com>
2025-10-08 22:57:13 +0700

    fix: bug type unknow when using evaluate validation

--- src/evaluate/evaluate.validation.ts
+++ src/evaluate/evaluate.validation.ts
@@ -1,7 +1,7 @@
-import { z, ZodType } from 'zod';
+import { z } from 'zod';
 
 export class EvaluateValidation {
-  static readonly START_EVALUATE: ZodType = z.object({
+  static readonly START_EVALUATE = z.object({
     job_title: z.string().min(1).max(100),
     cv_id: z.number().min(1).max(100).positive(),
     project_report_id: z.number().min(1).max(100).positive(),
